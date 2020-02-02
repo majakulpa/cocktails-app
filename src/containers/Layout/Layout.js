284429ced79ca9e9a3230d1ec19df14f6930f85d
majakulpa <majaweronikakulpa@gmail.com>
2020-02-02 19:21:39 +1100

    initial commit

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import classes from "./Layout.css";
+
+class Layout extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p className={classes.Red}>layout</p>
+      </div>
+    );
+  }
+}
+
+export default Layout;
