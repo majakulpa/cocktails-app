0d6be61fb2217fced11451b48c4f88a063750482
majakulpa <majaweronikakulpa@gmail.com>
2020-10-12 21:44:24 +1100

    add variables for colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./containers/Layout/Layout";
-import Main from "./components/Main/Main";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Main />
-      </Layout>
+      <Layout />
     </BrowserRouter>
   );
 }
